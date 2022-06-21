ccd3ae2a7c3ab9b1530239fa2ca17769350bddcf
Andrew Huang <huan1715@purdue.edu>
2022-06-21 10:32:45 +0800

    first

--- MiniProjectPath1.py
+++ MiniProjectPath1.py
@@ -1,5 +1,9 @@
 import pandas
-
+import matplotlib.pyplot as plt
+from datetime import datetime 
+import matplotlib.dates as mdates
+from matplotlib.dates import AutoDateLocator, AutoDateFormatter, date2num
+import numpy as np
 ''' 
 The following is the starting code for path1 for data reading to make your first step easier.
 'dataset_1' is the clean data for path1.
@@ -10,5 +14,45 @@ dataset_1['Manhattan Bridge']     = pandas.to_numeric(dataset_1['Manhattan Bridg
 dataset_1['Queensboro Bridge']    = pandas.to_numeric(dataset_1['Queensboro Bridge'].replace(',','', regex=True))
 dataset_1['Williamsburg Bridge']  = pandas.to_numeric(dataset_1['Williamsburg Bridge'].replace(',','', regex=True))
 dataset_1['Williamsburg Bridge']  = pandas.to_numeric(dataset_1['Williamsburg Bridge'].replace(',','', regex=True))
-print(dataset_1) #This line will print out your data
+#dataset_1['Date'].apply(lambda x: datetime.strptime(x, '%d/%m/%Y')) 
+# print(dataset_1.to_string()) #This line will print out your data
+date = dataset_1["Date"] + '-2016'
+date = pandas.to_datetime(date)
+highTemp = dataset_1['High Temp']
+lowTemp = dataset_1['Low Temp']
+brookBrd = dataset_1['Brooklyn Bridge']
 
+plt.figure(1)
+fig = plt.plot(date,highTemp)
+ax = plt.gca()
+plt.xticks(rotation=90)
+locator = AutoDateLocator()
+ax.xaxis.set_major_locator(locator)
+ax.xaxis.set_major_formatter(AutoDateFormatter(locator) )
+plt.xlabel('Time')
+plt.ylabel('Highest Temperature (Celcius)')
+plt.grid()
+
+plt.figure(2)
+fig = plt.plot(date,lowTemp)
+ax = plt.gca()
+plt.xticks(rotation=90)
+locator = AutoDateLocator()
+ax.xaxis.set_major_locator(locator)
+ax.xaxis.set_major_formatter(AutoDateFormatter(locator) )
+plt.xlabel('Time')
+plt.ylabel('Lowest Temperature (Celcius)')
+plt.grid()
+
+plt.figure(3)
+fig = plt.plot(date,brookBrd)
+ax = plt.gca()
+plt.xticks(rotation=90)
+locator = AutoDateLocator()
+ax.xaxis.set_major_locator(locator)
+ax.xaxis.set_major_formatter(AutoDateFormatter(locator) )
+plt.xlabel('Time')
+plt.ylabel('Bike Usage')
+plt.grid()
+
+plt.show()
